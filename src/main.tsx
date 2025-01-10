ab18d52faeb13449aeda57eba61b5119e879bd27
Ivan <ivanglzr01@gmail.com>
2025-01-10 23:01:18 +0100

    feat: implement log in view in log-in

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import LogInView from "./auth/views/LogInView.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/log-in" element={<LogInView />} />
     </Routes>
   </BrowserRouter>
 );
