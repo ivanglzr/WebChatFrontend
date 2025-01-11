26efb48a6c92f1e8902f764d28f8534346acf0a4
Ivan <ivanglzr01@gmail.com>
2025-01-11 15:31:12 +0100

    chore: improve auth schemas

--- src/auth/schemas/auth.ts
+++ src/auth/schemas/auth.ts
@@ -1,14 +1,28 @@
 import { z } from "zod";
 
 const emailSchema = z
-  .string()
+  .string({
+    required_error: "Email is required",
+    invalid_type_error: "Email must be a string",
+  })
   .email()
   .max(100, "Email can't be longer than 100 characters");
 
 const passwordSchema = z
-  .string()
+  .string({
+    required_error: "Password is required",
+    invalid_type_error: "Password must be a string",
+  })
   .min(6, "Password must be atleast 6 characters long")
   .max(100, "Password can't be longer than 100 characters");
+
+const fullnameSchema = z
+  .string({
+    required_error: "Name is required",
+    invalid_type_error: "Name must be a string",
+  })
+  .min(2, "Name must be atleast 2 characters long")
+  .max(50, "Name can't be longer than 50 characters");
 
 export const loginSchema = z.object({
   email: emailSchema,
@@ -16,7 +30,7 @@ export const loginSchema = z.object({
 });
 
 export const registerSchema = loginSchema.extend({
-  fullname: z.string(),
+  fullname: fullnameSchema,
 });
 
 export function validateLoginData(data: unknown) {
