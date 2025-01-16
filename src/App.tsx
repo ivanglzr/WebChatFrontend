85e2c866ef4fbf85391defb08cdcb47b80be8a44
Ivan <ivanglzr01@gmail.com>
2025-01-16 18:51:31 +0100

    feat: authenticate and connect socket on an effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { useEffect } from "react";
+
+import socket from "./socket";
+
+import useToken from "./auth/hooks/useToken";
+
 export default function App() {
-  return <p>Hello world</p>;
+  const token = useToken();
+
+  useEffect(() => {
+    socket.auth = { token };
+
+    if (!socket.connected) socket.connect();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [token]);
+
+  return <h1>Hello world</h1>;
 }
