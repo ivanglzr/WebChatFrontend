7ae5959f42f32b8ff120cfeaa45cb801b15507f9
Ivan <ivanglzr01@gmail.com>
2025-01-18 13:04:41 +0100

    refactor: update chat prop names

--- src/chat/types/chat.d.ts
+++ src/chat/types/chat.d.ts
@@ -1,11 +1,17 @@
 import type { UUID } from "crypto";
+import type { IUser } from "../../user/types/user";
 
 export interface ICreateChat {
   chatName: string;
-  usersIds: UUID[];
+  memberIds: UUID[];
 }
 
 export interface IChat extends ICreateChat {
   id: UUID;
   ownerId: UUID;
+  owner: IUser;
+}
+
+export interface IChatWithMembers extends IChat {
+  members: IUser[];
 }
