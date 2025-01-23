60f81cc8f7441ee527061ac53e42f5999605082a
Ivan <ivanglzr01@gmail.com>
2025-01-23 19:57:18 +0100

    feat: initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import socket from "./socket";
 
 import useToken from "./auth/hooks/useToken";
+import useChats from "./chat/hooks/useChats";
+
+import ChatsList from "./chat/components/ChatsList";
+import Chat from "./chat/components/Chat";
+
+import type { IChatWithMembers } from "./chat/types/chat";
 
 export default function App() {
   const token = useToken();
+  const { chats, loading, error } = useChats();
+
+  const [selectedChat, setSelectChat] = useState<IChatWithMembers | null>(null);
 
   useEffect(() => {
     socket.auth = { token };
@@ -17,5 +26,14 @@ export default function App() {
     };
   }, [token]);
 
-  return <h1>Hello world</h1>;
+  if (loading) return <span>Loading...</span>;
+  if (error) return <span>{error.message}</span>;
+  if (!chats) return <span>There isn&apos;t any chats</span>;
+
+  return (
+    <div className="grid grid-cols-[20%_auto] h-full">
+      <ChatsList chats={chats} setSelectedChat={setSelectChat} />
+      <Chat chat={selectedChat} />
+    </div>
+  );
 }
