d9b33073d706bc453d9fe5df41a0b1f00e30080e
Ivan <ivanglzr01@gmail.com>
2025-01-23 19:57:06 +0100

    chore: initial chat component

---
+++ src/chat/components/Chat.tsx
@@ -0,0 +1,24 @@
+import type { IChatWithMembers } from "../types/chat";
+
+interface Props {
+  chat: IChatWithMembers | null;
+}
+
+export default function Chat({ chat }: Props) {
+  if (!chat) return <div>Select a chat</div>;
+
+  return (
+    <main>
+      {chat.messages.map((message, index) => (
+        <span
+          className={`block w-full ${
+            message.sent ? "text-right" : "text-left"
+          }`}
+          key={index}
+        >
+          {message.content}
+        </span>
+      ))}
+    </main>
+  );
+}
