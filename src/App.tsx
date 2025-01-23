ae2ece945f48cfa80795707633dab39acb084aed
Ivan <ivanglzr01@gmail.com>
2025-01-23 22:28:18 +0100

    fix: remove unused effect from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import socket from "./socket";
-
-import useToken from "./auth/hooks/useToken";
 import useChats from "./chat/hooks/useChats";
 
 import ChatsList from "./chat/components/ChatsList";
@@ -11,20 +8,9 @@ import Chat from "./chat/components/Chat";
 import type { IChatWithMembers } from "./chat/types/chat";
 
 export default function App() {
-  const token = useToken();
   const { chats, loading, error } = useChats();
 
   const [selectedChat, setSelectChat] = useState<IChatWithMembers | null>(null);
-
-  useEffect(() => {
-    socket.auth = { token };
-
-    if (!socket.connected) socket.connect();
-
-    return () => {
-      socket.disconnect();
-    };
-  }, [token]);
 
   if (loading) return <span>Loading...</span>;
   if (error) return <span>{error.message}</span>;
