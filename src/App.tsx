7a4ee8e83a53aef74fc687d7cd399b31bd304692
Ivan <ivanglzr01@gmail.com>
2025-01-26 11:57:34 +0100

    feat: on message add it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import socket from "./socket";
+
+import { useEffect, useState } from "react";
 
 import useChats from "./chat/hooks/useChats";
+import useToken from "./auth/hooks/useToken";
 
 import ChatsList from "./chat/components/ChatsList";
 import Chat from "./chat/components/Chat";
@@ -8,9 +11,25 @@ import Chat from "./chat/components/Chat";
 import type { IChatWithMembers } from "./chat/types/chat";
 
 export default function App() {
-  const { chats, loading, error } = useChats();
+  const { chats, loading, error, addMessage } = useChats();
 
   const [selectedChat, setSelectChat] = useState<IChatWithMembers | null>(null);
+
+  const token = useToken();
+
+  useEffect(() => {
+    socket.auth = { token };
+
+    if (!socket.connected) socket.connect();
+
+    socket.on("Message created", addMessage);
+
+    return () => {
+      socket.off("Message created", addMessage);
+
+      socket.disconnect();
+    };
+  }, [token]);
 
   if (loading) return <span>Loading...</span>;
   if (error) return <span>{error.message}</span>;
