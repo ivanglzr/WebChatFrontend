7d95c38b40bc427e1632e7ef23ddf7097cc40b05
Ivan <ivanglzr01@gmail.com>
2025-02-08 15:08:19 +0100

    feat: scroll in chat

--- src/chat/components/Chat.tsx
+++ src/chat/components/Chat.tsx
@@ -8,7 +8,7 @@ export default function Chat({ chat }: Props) {
   if (!chat) return <div>Select a chat</div>;
 
   return (
-    <main>
+    <main className="overflow-y-auto">
       {chat.messages.map((message, index) => (
         <span
           className={`block w-full ${
