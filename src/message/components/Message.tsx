9942e9602a0c9c14e0b24c3652cbc9aa54d21f8b
Ivan <ivanglzr01@gmail.com>
2025-02-11 16:35:42 +0100

    fix: allow the span to adjust its height based on the text

--- src/message/components/Message.tsx
+++ src/message/components/Message.tsx
@@ -9,10 +9,10 @@ export default function Message({ message }: Props) {
     <li
       className={`${
         message.sent ? "text-right" : "text-left"
-      } my-2.5 last:mb-4`}
+      } my-0.5 last:mb-4`}
     >
       <span
-        className={`rounded-xl p-2 ${
+        className={`rounded-xl p-2 max-w-md inline-block text-left ${
           message.sent
             ? "border-2 border-primary"
             : "bg-primary border-2 border-transparent text-white"
